9500619e23f487ba68de48abacecbf06a02ed03b
Eliyahu <mr.gorden.smit@gmail.com>
2020-08-13 22:54:26 +0300

    Almost done, need to fix one bug with cheat block

--- UWpartC/hw6/hw6assignment.rb
+++ UWpartC/hw6/hw6assignment.rb
@@ -1,32 +1,28 @@
+# Homework assignment 6 | UWpartC | by SID 2020
 # University of Washington, Programming Languages, Homework 6, hw6runner.rb
 
-# This is the only file you turn in, so do not modify the other files as
-# part of your solution.
+## TODO fix bug when cheat block stays on the screen
 
 class MyPiece < Piece
-
-  def initialize (point_array,board)
-    super
-  end
 
   def self.next_piece (board)
     MyPiece.new(All_My_Pieces.sample, board)
   end
 
-  All_My_Pieces = All_Pieces + [
+  def self.next_cheat_piece (board)
+    MyPiece.new([[0,0]], board)
+  end
+
+  # adds 3 more pieces
+  All_My_Pieces =
+    All_Pieces + [
     [[[0, 0], [-1, 0], [1, 0], [2, 0], [-2, 0]],         # 5 blocks line
      [[0, 0], [0, -1], [0, 1], [0, 2], [0, -2]]],
-    rotations([[2, 0], [0, 0], [1, 0], [0, 1], [1, 1]]), # square with appendicitis
+    rotations([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]]), # square with appendicitis
     rotations([[0, 0], [0, 1], [1, 0]]) ]                # 3 block corner
-
 end
 
 class MyBoard < Board
-
-  def initialize(game)
-    super
-    @current_block = MyPiece.next_piece(self)
-  end
 
   # rotates the current piece 180 degrees
   def rotate_180
@@ -36,9 +32,24 @@ class MyBoard < Board
     draw
   end
 
+  # checks if player has cheated and gets next piece
   def next_piece
-    @current_block = MyPiece.next_piece(self)
-    @current_pos = nil
+    if @is_cheated
+    then
+      @current_block = MyPiece.next_cheat_piece(self)
+      @is_cheated = false
+    else
+      @current_block = MyPiece.next_piece(self)
+    end
+      @current_pos = nil
+  end
+
+  # decreases score by 100 if player cheated
+  def cheat
+    if !@is_cheated and @score >= 100
+      @score -= 100
+      @is_cheated = true
+    end
   end
 
   def store_current
@@ -53,14 +64,10 @@ class MyBoard < Board
     remove_filled
     @delay = [@delay - 2, 80].max
   end
+
 end
 
 class MyTetris < Tetris
-
-  def initialize
-    super
-    my_key_bindings
-  end
 
   def set_board
     @canvas = TetrisCanvas.new
@@ -70,10 +77,10 @@ class MyTetris < Tetris
     @board.draw
   end
 
-  def my_key_bindings
+  def key_bindings
+    super
     @root.bind('u', proc {@board.rotate_180})
-    # for cheat block
-    @root.bind('c', proc {})
+    @root.bind('c', proc {@board.cheat})
   end
 
 end
